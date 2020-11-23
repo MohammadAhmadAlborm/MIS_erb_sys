40dcdbaa1cc3bd1a97b06d1fe60c8510d6e9b5ba
mohammad <ma777414@gmail.com>
2020-11-23 19:38:37 +0300

    added new file

--- sales/routes/web.php
+++ sales/routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blogSingle', function () {
+    return view('blog-single');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/contactus', function () {
+    return view('contact-us');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/productDetails', function () {
+    return view('product-details');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
